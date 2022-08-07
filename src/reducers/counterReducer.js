27aa4f89e5fe45bc8583b42c9d73a583b2071bc9
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2022-08-07 17:49:36 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,11 @@
 const counterReducer = (state,action) =>{
-
+switch (action.type){
+    case "increment";
+    return state+1;
+     case "decrement";
+    return state-1;
+  default:
+    throw new Error();
 }
 
 export {counterReducer}
